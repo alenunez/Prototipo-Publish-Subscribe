c5f47cd4fefec83add74f010c6c15a1008c57c46
Alejandro Nuñez Herrera <alejandro2191947@correo.uis.edu.co>
2023-06-23 21:29:34 -0500

    Modificaiones

--- Publicador2.py
+++ Publicador2.py
@@ -6,8 +6,7 @@ channel = connection.channel()
 channel.queue_declare(queue='cola2')
 
 message = 'Mensaje del publicador 2'
-channel.basic_consume(queue='cola1', on_message_callback=callback, auto_ack=True)
-channel.basic_consume(queue='cola2', on_message_callback=callback, auto_ack=True)
-channel.basic_consume(queue='cola3', on_message_callback=callback, auto_ack=True)print("Mensaje enviado por el publicador 2: %r" % message)
+channel.basic_publish(exchange='', routing_key='cola2', body=message)
+print("Mensaje enviado por el publicador 2: %r" % message)
 
 connection.close()
--- Suscribtor1.py
+++ Suscribtor1.py
@@ -7,15 +7,10 @@ channel = connection.channel()
 
 channel.queue_declare(queue='cola1')
 
-
 def callback(ch, method, properties, body):
     print("Mensaje recibido por el suscriptor 1: %r" % body)
 
 channel.basic_consume(queue='cola1', on_message_callback=callback, auto_ack=True)
-channel.basic_consume(queue='cola2', on_message_callback=callback, auto_ack=True)
-channel.basic_consume(queue='cola3', on_message_callback=callback, auto_ack=True)
-
-
 
 print('Esperando mensajes...')
 channel.start_consuming()
